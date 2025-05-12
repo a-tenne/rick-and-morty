28930b117fea4e168798ac21a6a22d190459fe31
a-tenne <atenne@web.de>
2025-05-12 18:18:35 +0200

    pagination fix & favicon

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -18,21 +18,29 @@ import { RouterLink } from '@angular/router';
 export class PaginationComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['currentPageNum'] || changes['pageInfo']) {
-      const nums = [],
+      const nums1: number[] = [],
+        nums2: number[] = [],
         max = this.pageInfo.pages;
+
+      for (let i = this.currentPageNum - 1, j = 0; i > 0 && j < 2; --i, ++j) {
+        nums1.unshift(i);
+      }
       for (
-        let i = this.currentPageNum + 1, j = 1;
-        i <= max && j <= 5;
+        let i = this.currentPageNum + 1, j = 0;
+        i <= max && j < 2;
         ++i, ++j
       ) {
-        nums.push(i);
+        nums2.push(i);
       }
-      this.nextThree.set(nums);
+
+      this.prevTwo.set(nums1);
+      this.nextTwo.set(nums2);
     }
   }
   @Input()
   pageInfo!: PageInfo;
   @Input()
   currentPageNum!: number;
-  nextThree = signal<number[]>([]);
+  prevTwo = signal<number[]>([]);
+  nextTwo = signal<number[]>([]);
 }
